853bf69030de9e98f6782eb1924fd7aee5e37ce8
subhadeep <srsubhadeepghosh456@gmail.com>
2024-02-29 12:39:52 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running!!");
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database conneted");
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running!!");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
