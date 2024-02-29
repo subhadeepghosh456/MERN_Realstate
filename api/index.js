e07dbf49af10ff66d60012801a031fe1ef2ef454
subhadeep <srsubhadeepghosh456@gmail.com>
2024-02-29 20:48:42 +0530

    signup route created

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoute.js";
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -21,3 +24,4 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
