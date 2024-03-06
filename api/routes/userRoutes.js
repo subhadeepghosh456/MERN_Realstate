5e55dc794a3b02d6cb71a6e430e62cf596d9a1f8
subhadeep <srsubhadeepghosh456@gmail.com>
2024-03-06 13:41:00 +0530

    create get user listings api route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUserInfo,
   deleteUser,
+  getUserListings,
 } from "../controllers/userControllers.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUserInfo);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -158,7 +158,7 @@ const CreateListing = () => {
       if (data.success === false) {
         setError(data.message);
       }
-      navigate(`/listing/${data._id}`);
+      navigate(`/listing/${currentUser._id}`);
     } catch (error) {
       setError(error.message);
       setLoading(false);
