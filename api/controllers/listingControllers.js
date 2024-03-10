990b415a4e687df0a2e5005c83604ee9b54ba4a2
subhadeep <srsubhadeepghosh456@gmail.com>
2024-03-10 13:42:59 +0530

    add functionality to search pagr

--- api/controllers/listingControllers.js
+++ api/controllers/listingControllers.js
@@ -65,15 +65,15 @@ export const getListings = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
-    if (offer === undefined || offer === false) {
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
     let furnished = req.query.furnished;
-    if (furnished === undefined || furnished === false) {
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
     let parking = req.query.parking;
-    if (parking === undefined || parking === false) {
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
     let type = req.query.type;
