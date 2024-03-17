8e81933302200fb6a0c07b95a165c706515af80b
subhadeep <srsubhadeepghosh456@gmail.com>
2024-03-17 20:46:18 +0530

    final upload

--- api/index.js
+++ api/index.js
@@ -5,7 +5,10 @@ import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingRoute.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,6 +32,12 @@ mongoose
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
